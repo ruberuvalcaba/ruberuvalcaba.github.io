8922459bc28f84108fa59b7305fe3b0c9e01a355
Ruben Flores <ruberuvalcaba@gmail.com>
2026-08-20 14:05:29 -0400

    Fixes import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import Router from "./router";
+import Router from "./Router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
