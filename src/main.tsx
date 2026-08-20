92ec585535fccd0dc55aa34081de76252766d90b
Ruben Flores <ruberuvalcaba@gmail.com>
2026-08-20 15:18:08 -0400

    Adds 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import Router from "./Router";
 
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router />
